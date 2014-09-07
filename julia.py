3a3cc8dafa61d26a29b421958514ebbff34f248b
ashwin <ashwinashok9111993@gmail.com>
2014-09-08 00:33:25 +0530

    updates

--- julia.py
+++ julia.py
@@ -9,21 +9,23 @@ import pycuda.gpuarray as gpuarray
 from pycuda.compiler import SourceModule
 import pycuda.curandom
 import pycuda.cumath
+import matplotlib.pyplot as p
 
 mod = SourceModule(
 """
 __global__ void julia(float *pix)
-#include<math.h>
+
 #define INDEX(a, b) a*256+b
 {
   int i = threadIdx.x+(blockIdx.x*(blockDim.x));
   int j = threadIdx.y+(blockIdx.y*(blockDim.y));
 
   float newRe, newIm, oldRe, oldIm;
-  float  cRe = -0.7;
-  float  cIm = 0.270;
-  int maxIterations = 300;
-
+  float  cRe = -0.01;
+  float  cIm = 0.7;
+  int maxIterations = 200;
+  newRe =   0.05*(i)/12-0.05*12;
+  newIm =   0.05*(j)/12;
   int id;
         //start the iteration process
         for(id = 0; id < maxIterations; id++)
@@ -35,12 +37,14 @@ __global__ void julia(float *pix)
             newRe = oldRe * oldRe - oldIm * oldIm + cRe;
             newIm = 2 * oldRe * oldIm + cIm;
             //if the point is outside the circle with radius 2: stop
-            if((newRe * newRe + newIm * newIm) > 4) break;
+            if((newRe * newRe + newIm * newIm) > 3)
+            {
+            break;
+            }
         }
-    pix[INDEX(i,j)] = id;
+        pix[INDEX(i,j)] = id;
 
-  i += blockDim.x * gridDim.x;
-  j += blockDim.y * gridDim.y;
+
 
 }
 """)
@@ -49,5 +53,9 @@ julia = mod.get_function("julia")
 
 M=256
 N=256
-pix = np.zeros(M*N)
-
+pix = np.zeros(M*N,order='F').astype(np.float32)
+julia(drv.InOut(pix),block=(32,32, 1), grid=(8,8, 1))
+pix=np.reshape(pix,(M,N), order='F').astype(np.float32)
+print pix
+p.imshow(pix)
+p.show()
