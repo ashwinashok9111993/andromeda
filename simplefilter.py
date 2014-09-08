c6987610046c0f98104a767454357d0d487514f3
ashwin <ashwinashok9111993@gmail.com>
2014-09-09 00:58:08 +0530

    updates

--- simplefilter.py
+++ simplefilter.py
@@ -9,7 +9,7 @@ import scipy.misc as scm
 import matplotlib.pyplot as p
 
 mod = SourceModule("""
-__global__ void rgb2gray(float *C,float *dest,float *r_img, float *g_img, float *b_img)
+__global__ void rgb2gray(float *C,float *dest,float *r_img)
 {
 int a,b;
 int M=C[0];
@@ -21,8 +21,8 @@ while(a<M){
 
 while(b<N){
 
-dest[a*N+b] = (0.299*r_img[a*N+b]+0.587*g_img[a*N+b]+0.114*b_img[a*N+b]);
-__syncthreads();
+dest[a*N+b] = (r_img[(a)*N+b])+(r_img[(a)*N+b]);
+
 b += blockDim.y*gridDim.y;
 }
 a += blockDim.x*gridDim.x;
@@ -30,18 +30,14 @@ a += blockDim.x*gridDim.x;
 }
 """)
 
-a = scm.imread('dice.png').astype(np.float32)updates
-(M,N,L)=a.shape
+a = scm.imread('lenaG.jpg').astype(np.float32)
+(M,N)=a.shape
 print M,N
-
-r_img = a[:, :, 0].reshape(M*N, order='F')
-g_img = a[:, :, 1].reshape(M*N, order='F')
-b_img = a[:, :, 2].reshape(M*N, order='F')
+r_img = a.reshape(M*N, order='F')
 dest=np.zeros_like(r_img)
-print dest
 
 rgb2gray = mod.get_function("rgb2gray")
-rgb2gray(drv.In(np.float32([M,N])),drv.Out(dest), drv.In(r_img), drv.In(g_img),drv.In(b_img),block=(32, 32, 1), grid=(M/32,N/32,1))
+rgb2gray(drv.In(np.float32([M,N])),drv.Out(dest), drv.In(r_img),block=(32, 32, 1), grid=(M/32,N/32,1))
 
 dest=np.reshape(dest,(M,N), order='F')
 
